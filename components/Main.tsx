df25773b7c5837c5fcf28b00f2bd347cdae3c8fa
hyerijung <hyerijung0903@gmail.com>
2025-06-21 10:42:20 +0900

    fix date text

--- components/Main.tsx
+++ components/Main.tsx
@@ -14,10 +14,10 @@ export default function Main() {
         <source src="/bilingual.MP4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
+
       {/* 배경 오버레이 추가 (텍스트 가독성을 위해) */}
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-      
+
       {/* 콘텐츠를 오버레이 위에 표시 */}
       <div className="relative z-1 flex flex-col items-center">
         <div className="font-sans text-2xl text-white">You're invited to</div>
@@ -25,13 +25,13 @@ export default function Main() {
           Bilingual Retreat
         </h1>
         <p className="font-sans text-xl text-white mb-12 font-light">
-          Saturday, June 28th, 2025
+          Saturday 28th June 2025
         </p>
       </div>
-      
+
       {/* 아래 방향 화살표 설정) */}
       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 cursor-pointer animate-bounce z-10">
-       <svg
+        <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-10 w-10 text-white"
           fill="none"
